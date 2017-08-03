922e9eef2ca876393687071ece6186d6f26c0804
Sanjay Purswani <sanjsanj@hotmail.com>
2017-08-03 23:34:45 +0800

    Homogenise expects in tests, add actual image URLs

--- spec/createNode.spec.js
+++ spec/createNode.spec.js
@@ -3,67 +3,67 @@ import ipsum from "./helpers/ipsum";
 
 describe("createNode function", () => {
     it("can create a node of type 'item'", () => {
-        const actual = {type: "item"};
+        const actual = createNode({type: "item"});
 
         let expected = document.createElement("div");
         expected.className = "item";
 
-        expect(createNode(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 
     it("can create a node of type 'image'", () => {
-        const actual = {
+        const actual = createNode({
             type: "image",
             item: {
                 volumeInfo: {
                     title: "Image title",
                     imageLinks: {
-                        smallThumbnail: "www.image.com/image.jpg",
+                        smallThumbnail: "http://lorempixel.com/5/5/",
                     },
                 },
             },
-        };
+        });
 
         let expected = document.createElement("img");
         expected.className = "image";
-        expected.src = "www.image.com/image.jpg";
+        expected.src = "http://lorempixel.com/5/5/";
         expected.alt = "Image title";
 
-        expect(createNode(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 
     it("can create a node of type 'title'", () => {
-        const actual = {
+        const actual = createNode({
             type: "title",
             item: {
                 volumeInfo: {
                     title: "Title",
                 },
             },
-        };
+        });
 
         let expected = document.createElement("p");
         expected.className = "title";
         expected.innerHTML = "Title";
 
-        expect(createNode(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 
     it("can create a node of type 'description'", () => {
-        const actual = {
+        const actual = createNode({
             type: "description",
             item: {
                 volumeInfo: {
                     description: ipsum,
                 },
             },
-        };
+        });
 
         let expected = document.createElement("p");
         expected.className = "description";
         const ipsum200 = ipsum.substring(0, 199);
         expected.innerHTML = `${ipsum200}...`;
 
-        expect(createNode(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });
 });
